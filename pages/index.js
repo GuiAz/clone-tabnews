0bbc7d072851c03ad9bc53966166dcd6e6f1be59
GuiAz <138117436+GuiAz@users.noreply.github.com>
2026-01-03 14:04:28 +0000

    adiciona scripts lint:check e lint:fix

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
-
 function Home() {
-    return <h1>Nicolle, eu amo você. Se você me ama, dá uma risadinha! :)</h1>
+  return <h1>Nicolle, eu amo você. Se você me ama, dá uma risadinha! :)</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
