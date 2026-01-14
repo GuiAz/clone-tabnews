2d9497fee95aa8db587e92f4136a758c98b46a3a
GuiAz <138117436+GuiAz@users.noreply.github.com>
2026-01-14 11:43:16 +0000

    Minha resposta antes da resposta do Felipe

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,23 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 +1 as sum");
-  console.log(result.rows);
-  response.status(200).json({ chave: "são acima da média" });
+  const updateAt = new Date().toISOString();
+  const versaoPostgres = await database.query("SELECT version()");
+  const conectionsMax = await database.query("SHOW max_connections");
+  const conectionsUsed = await database.query(
+    "SELECT count(*) FROM pg_stat_activity;",
+  );
+
+  console.log(versaoPostgres.rows[0].version);
+  console.log(conectionsMax.rows[0].max_connections);
+  console.log(conectionsUsed.rows[0]);
+
+  response.status(200).json({
+    update_at: updateAt,
+    versao_postgres: versaoPostgres.rows[0].version,
+    conections_max: conectionsMax.rows[0].max_connections,
+    conections_used: conectionsUsed.rows[0],
+  });
 }
 
 export default status;
