b5722e746e8c32d013e70eb334e7e11c9fdfc081
Guilherme Cottas <guicottas@hotmail.com>
2026-02-01 13:26:55 +0000

    feat: add initial version of /status page

---
+++ pages/status/index.js
@@ -0,0 +1,62 @@
+import useSWR from "swr";
+
+async function fetchAPI(key) {
+  const response = await fetch(key);
+  const responseBody = await response.json();
+  return responseBody;
+}
+
+export default function StatusPage() {
+  return (
+    <>
+      <h1>Status</h1>
+      <UptadedAt />
+      <DatabaseStatus />
+    </>
+  );
+}
+
+function UptadedAt() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
+
+  let uptadedAtText = "Carregando...";
+
+  if (!isLoading && data) {
+    uptadedAtText = new Date(data.updated_at).toLocaleString("pt-BR");
+  }
+
+  return <div>Última atualização: {uptadedAtText}</div>;
+}
+
+function DatabaseStatus() {
+  const { isLoading, data } = useSWR("/api/v1/status/", fetchAPI, {
+    refreshInterval: 2000,
+  });
+
+  let databaseStatusInformation = "Carregando...";
+
+  if (!isLoading && data) {
+    databaseStatusInformation = (
+      <>
+        <div>Versão: {data.dependecies.database.version}</div>
+        <div>
+          Conexões abertas: {data.dependecies.database.opened_connections}
+        </div>
+        <div>
+          Conexões Máximas: {data.dependecies.database.max_connnections}
+        </div>
+      </>
+    );
+
+    return (
+      <>
+        <h2>Database</h2>
+        <div>{databaseStatusInformation}</div>
+      </>
+    );
+  }
+
+  return <div>{databaseStatusInformation}</div>;
+}
