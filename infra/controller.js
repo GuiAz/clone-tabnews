4d818150edea860b0d44b4722109c77c169c4454
Guilherme Cottas <guicottas@hotmail.com>
2026-02-06 16:01:33 +0000

    feat: create and user ValidationError in controller.js

--- infra/controller.js
+++ infra/controller.js
@@ -1,6 +1,14 @@
-import { InternalServerError, MethodNotAllowedError } from "infra/errors";
+import {
+  InternalServerError,
+  MethodNotAllowedError,
+  ValidationError,
+} from "infra/errors";
 
 function onErrorHandler(error, request, response) {
+  if (error instanceof ValidationError) {
+    return response.status(error.statusCode).json(error);
+  }
+
   const publicErrorObject = new InternalServerError({
     statusCode: error.statusCode,
     cause: error,
@@ -15,6 +23,7 @@ function onNoMatchHandler(request, response) {
   const publicErrorObject = new MethodNotAllowedError();
   response.status(publicErrorObject.statusCode).json(publicErrorObject);
 }
+
 const controller = {
   errorHandlers: {
     onNoMatch: onNoMatchHandler,
