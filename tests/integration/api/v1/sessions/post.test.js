dca1cdfbedb324d152b69c5abbd3853fdd1d794b
Guilherme Cottas <guicottas@hotmail.com>
2026-02-13 11:17:51 +0000

    feat: implement /api/v1/user endpoint with session reveal

--- tests/integration/api/v1/sessions/post.test.js
+++ tests/integration/api/v1/sessions/post.test.js
@@ -142,7 +142,7 @@ describe("POST /api/v1/sessions", () => {
 
       expect(parsedSetCookie.session_id).toEqual({
         name: "session_id",
-        value: responseBody.token,
+        value: sessionObject.token,
         maxAge: session.EXPIRATION_IN_MILISECONDS / 1000,
         path: "/",
         httpOnly: true,
